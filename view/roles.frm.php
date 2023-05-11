f9461cfe318fe23c0c313b67e05db5233139b4c0
neido6 <neido6@gmail.com>
2023-05-10 19:12:08 -0500

    Primer Commit MVC

---
+++ view/roles.frm.php
@@ -0,0 +1,20 @@
+<?php include_once "header.php"; ?>
+<div class="row my-5">
+    <h1 class="text-center">Formulario Roles</h1>
+</div>
+<div class="row">
+    <div class="col-3 d-flex justify-content-end align-items-center">
+        <span class="fw-bolder">Nombre Rol:</span>
+    </div>
+    <div class="col-7">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="floatingInput" placeholder="admin">
+            <label for="floatingInput">Nombre Rol</label>
+        </div>
+    </div>
+</div>
+<div class="row mt-5 bg-dark">
+    <h1 class="text-white text-center">Tabla de Roles</h1>
+</div>
+
+<?php include_once "footer.php"; ?>
